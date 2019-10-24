a0827a1fa2e5074230e43304b846d6b9647442fb
Davide Sovrano <davidesovrano@MacBook-Pro-di-Davide.local>
2019-10-24 09:55:27 +0200

    post update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_154426) do
+ActiveRecord::Schema.define(version: 2019_10_22_194753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_10_21_154426) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "titolo"
+    t.text "corpo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
